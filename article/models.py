e1e40eb43f45b6c141accbcff06bb6584802609c
JakeLin <www95175300lin@163.com>
2020-04-06 10:41:17 +0800

    Version-0.1

--- article/models.py
+++ article/models.py
@@ -4,6 +4,19 @@ from django.utils import timezone
 from django.urls import reverse
 
 
+class ArticleColumn(models.Model):
+    """
+    栏目的 Model
+    """
+    # 栏目标题
+    title = models.CharField(max_length=100, blank=True)
+    # 创建时间
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
 class ArticlePost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -11,6 +24,13 @@ class ArticlePost(models.Model):
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     total_views = models.PositiveIntegerField(default=0)
+    column = models.ForeignKey(
+        ArticleColumn,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='article'
+    )
 
     class Meta:
         ordering = ('-created',)
