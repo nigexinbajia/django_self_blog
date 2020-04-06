14a2f2054a3fd158e3872aba842c288b668d2e51
JakeLin <www95175300lin@163.com>
2020-04-06 10:56:49 +0800

    Version-0.1

--- article/models.py
+++ article/models.py
@@ -19,11 +19,6 @@ class ArticleColumn(models.Model):
 
 class ArticlePost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
-    updated = models.DateTimeField(auto_now=True)
-    total_views = models.PositiveIntegerField(default=0)
     column = models.ForeignKey(
         ArticleColumn,
         null=True,
@@ -31,6 +26,11 @@ class ArticlePost(models.Model):
         on_delete=models.CASCADE,
         related_name='article'
     )
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    total_views = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ('-created',)
